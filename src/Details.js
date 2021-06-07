4022c0bd1782c94f655c3f4f36cb9a7a31d5ac11
Obinna Ogbonna <oogbonna@gidimobile.com>
2021-06-07 08:25:49 +0100

    add an error handler

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,46 @@
-import { useParams } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
-const Details = () => {
-    const { id } = useParams();
-    return <h2>{id}</h2>;
+class Details extends Component {
+    state = { loading: true };
+
+    async componentDidMount() {
+        const res = await fetch(
+            `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+        );
+        const json = await res.json();
+        this.setState(Object.assign({ loading: false }, json.pets[0]))
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h2>loading ...</h2>
+        }
+
+        const { animal, breed, city, state, description, name, images } = this.state;
+
+        return (
+            <div className="details">
+                <Carousel images={images} />
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Details;
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <DetailsWithRouter {...props} />
+        </ErrorBoundary>
+    )
+}
