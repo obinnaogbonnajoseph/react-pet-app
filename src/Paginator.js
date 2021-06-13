3d1617ec1a048a54c98e3946a6bbcb2fe38f5a51
Obinna Ogbonna <oogbonna@gidimobile.com>
2021-06-13 09:08:42 +0100

    style paginator

--- src/Paginator.js
+++ src/Paginator.js
@@ -2,9 +2,13 @@ const Paginator = ({ page, hasNext, handlePageChange }) => {
     let newPage = page;
     return (
         <div className="paginator-wrapper">
-            <button disabled={page <= 0} onClick={(_) => handlePageChange(newPage - 1)} className="tab-left">&#60;</button>
-            <span className="currentPage">{newPage + 1}</span>
-            <button disabled={!hasNext} onClick={(_) => handlePageChange(newPage + 1)} className="tab-right">&#62;</button>
+            <div className="paginator-left"></div>
+            <div className="page-buttons">
+                <button disabled={page <= 0} onClick={(_) => handlePageChange(newPage - 1)} className="tab-left">&#60;</button>
+                <span className="currentPage">{newPage + 1}</span>
+                <button disabled={!hasNext} onClick={(_) => handlePageChange(newPage + 1)} className="tab-right">&#62;</button>
+            </div>
+
         </div>
     )
 }
